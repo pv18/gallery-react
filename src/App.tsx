83e174779766a21b9957858b1cc5297be551a15b
pv18 <pv9998286620@yandex.ru>
2022-09-12 14:32:43 +0300

    added all photos to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {IPhoto} from './types/shared';
+
 
 const App = () => {
+    const [photos, setPhotos] = useState<IPhoto[]>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(response => response.json())
+            .then(data => setPhotos(data))
+    }, [])
+
     return (
-        <div>
-          
-        </div>
+        <Container>
+            <Grid container spacing={1}>
+                {photos.map(photo => (
+                    <Grid key={crypto.randomUUID()} item xs={4} md={3} lg={2}>
+                        <Card sx={{maxWidth: 345}}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={photo.thumbnailUrl}
+                                    alt={'photo'}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {photo.title}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     );
 };
 
